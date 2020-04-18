2b62555290d2738df957693d8ec727aca86ed2af
tanmoyopenroot <tanmoy.openroot@gmail.com>
2020-04-18 19:37:05 +0530

    test(button): add test for button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -8,7 +8,7 @@ export interface IButtonProps {
 export const Button = React.memo(styledComponents.button<IButtonProps>`
   background-image: ${props => props.theme.ACTIONABLE_BACKGROUND_IMAGE};
   color: ${props => props.theme.PRIMARY_TEXT};
-  font-size: ${props => props.theme.FONT.MEDIUM};
+  font-size: ${props => props.theme.FONT && props.theme.FONT.MEDIUM};
   border-radius: ${props => props.theme.RADIUS};
   width: ${props => props.width || '100%'};
   border: none;
---
+++ src/components/Button/__test__/Button.spec.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { mount } from 'enzyme';
+
+import {
+  Button,
+  IButtonProps,
+} from '../Button';
+
+describe('Testing <Button />', () => {
+  const handleOnClick = jest.fn();
+  const props: IButtonProps = {
+    width: '100px',
+  };
+
+  const wrapper = mount<IButtonProps, {}>(
+    (
+      <Button
+        {...props}
+        onClick={handleOnClick}
+      >
+        Click Me!
+      </Button>
+    ),
+  );
+
+  it('should be able to render with props', () => {
+    expect(wrapper.length).toEqual(1);
+    expect(wrapper.children().length).toBe(1);
+    expect(wrapper.text()).toEqual('Click Me!');
+    expect(wrapper.props().width).toEqual(props.width);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should be able to invoke onClick', () => {
+    wrapper.simulate('click');
+    expect(handleOnClick).toBeCalledTimes(1);
+  });
+});
