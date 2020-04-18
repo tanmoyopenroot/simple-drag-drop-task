b618f164ccc7ba12dd6b282f0b532d97d36059bf
tanmoyopenroot <tanmoy.openroot@gmail.com>
2020-04-18 20:19:00 +0530

    test(input): fix test for input component

--- src/components/Input/Input.styles.tsx
+++ src/components/Input/Input.styles.tsx
@@ -2,7 +2,7 @@ import styledComponents from 'styled-components';
 
 export const InputElement = styledComponents.input`
   color: ${props => props.theme.PRIMARY_TEXT};
-  font-size: ${props => props.theme.FONT.MEDIUM};
+  font-size: ${props => props.theme.FONT && props.theme.FONT.MEDIUM};
   width: 95%;
   border: 0px;
   outline: 0px;
